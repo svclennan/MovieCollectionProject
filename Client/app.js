95bc8c09e33dfac71d2db8ee4688c2827320b670
unknown <svclennan@icloud.com>
2020-03-03 11:46:18 -0600

    clicking the name of the movie allows the user to edit the movies details. passes the correct values into the edit area

--- Client/app.js
+++ Client/app.js
@@ -31,7 +31,7 @@
             success: resetTable
         })
     }
-
+    
     function resetTable(data){
         if(data == null){
             var list = [];
@@ -46,15 +46,54 @@
         var movieTable = "";
         $.each(list, function (index, movie) {
             movieTable += '<tr>';
-            movieTable += '<td>' + '<a onclick= stuff() href="#" id="myBtn">' + movie.title + '</a></td>';
+            movieTable += '<td>' + '<a ' + 'onclick = editMovie(' + movie["movieId"] + ')' + ' href=#' + ' id=myBtn>' + movie.title + '</a></td>';
             movieTable += '<td>' + movie.genre + '</td>';
             movieTable += '<td>' + movie.director + '</td>';
             movieTable += '</tr>'
         });
         $('#movies').append(movieTable);
     }
+    
+    function clearForm(){
+        var form = document.getElementById("my-form");
+        form.reset();
+    }
 
     $(document).ready(makeTable);
     $('#my-form').submit(processForm);
-    $('#my-form').submit(makeTable);
+    $('#submit').on("click", makeTable);
 })(jQuery); 
+
+function editMovie(id){
+    $.get(("https://localhost:44325/api/movie/"+id), function(data){
+        changeDetails(data["title"], data["genre"], data["director"])
+    });
+}
+
+function changeDetails(title, genre, director){
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+    var btn = document.getElementById("myBtn");
+    var closeButton = document.getElementById("closeButton");
+    var titleInput = document.getElementById("titleInput");
+    var genreInput = document.getElementById("genreInput");
+    var directorInput = document.getElementById("directorInput");
+    modal.style.display = "block";
+
+    titleInput.value = title;
+    genreInput.value = genre;
+    directorInput.value = director;
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    closeButton.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
