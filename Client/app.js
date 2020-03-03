e65343f62e24ad1db45ae82c53b241c8bdb9bbb3
Zac Melton <zac.melton@outlook.com>
2020-03-03 15:02:45 -0600

    Added the ability to edit a movie

--- Client/app.js
+++ Client/app.js
@@ -46,7 +46,7 @@
         var movieTable = "";
         $.each(list, function (index, movie) {
             movieTable += '<tr>';
-            movieTable += '<td>' + '<a ' + 'onclick = editMovie(' + movie["movieId"] + ')' + ' href=#' + ' id=myBtn>' + movie.title + '</a></td>';
+            movieTable += '<td>' + '<a ' + 'onclick = editMovie(' + movie["movieId"] + ')' + ' href="#">' + movie.title + '</a></td>';
             movieTable += '<td>' + movie.genre + '</td>';
             movieTable += '<td>' + movie.director + '</td>';
             movieTable += '</tr>'
@@ -54,6 +54,29 @@
         $('#movies').append(movieTable);
     }
     
+    function submitEditChanges(){
+        var dict = {
+            Title: this["title"].value,
+            Genre: this["genre"].value,
+            Director: this["director"].value,
+            MovieId: this["movieId"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie/?id='+dict.MovieId,
+            dataType: 'text',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function (data, textStatus, jQxhr) {
+                $('#response pre').html(data);
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+        });
+    }
+
     function clearForm(){
         var form = document.getElementById("my-form");
         form.reset();
@@ -61,28 +84,31 @@
 
     $(document).ready(makeTable);
     $('#my-form').submit(processForm);
-    $('#submit').on("click", makeTable);
+    //$('#submit').on("click", makeTable);
+    $('#my-edit-form').submit(submitEditChanges);
 })(jQuery); 
 
 function editMovie(id){
     $.get(("https://localhost:44325/api/movie/"+id), function(data){
-        changeDetails(data["title"], data["genre"], data["director"])
+        changeDetails(data["title"], data["genre"], data["director"], data["movieId"])
     });
 }
 
-function changeDetails(title, genre, director){
+function changeDetails(title, genre, director, id){
     var modal = document.getElementById("myModal");
     var span = document.getElementsByClassName("close")[0];
-    var btn = document.getElementById("myBtn");
     var closeButton = document.getElementById("closeButton");
     var titleInput = document.getElementById("titleInput");
     var genreInput = document.getElementById("genreInput");
     var directorInput = document.getElementById("directorInput");
+    var idInput = document.getElementById("idInput");
     modal.style.display = "block";
 
+    idInput.value = id;
     titleInput.value = title;
     genreInput.value = genre;
     directorInput.value = director;
+    
     span.onclick = function() {
         modal.style.display = "none";
     }
